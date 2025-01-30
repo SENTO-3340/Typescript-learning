a1cd1f417c2f8462ef442f097f0a5ccb4075d29c
hijiri <crew.sento0864@gmail.com>
2025-01-31 00:20:49 +0900

    更新

--- learning-project/src/section/class.ts
+++ learning-project/src/section/class.ts
@@ -6,12 +6,11 @@ export const cls = () => {
 
   //まずclass名を定義(ここで同時に同じ名前の型も作られる)
   class Person {
-
-    //constructor内で使用するプロパティの型注釈
-    //private装飾子をつけるとclassの外からアクセスできなくなる
+    //constructor内で使用するプロパティの型注釈（フィールド）
+    //public装飾子（省略可）、これはデフォルト。どこでもアクセス可。
+    public name: string;
+    //private装飾子、classの外からアクセスできなくなる
     //class内でしか使用せず、呼び出し時に変更したくない時などに使用
-    //逆にpublic装飾子もあり、これはデフォルト。どこでもアクセス可。
-    name: string; 
     private age: number;
 
     // constructor関数（オブジェクトの初期化処理をする）
@@ -19,29 +18,44 @@ export const cls = () => {
     constructor(initName: string, initAge: number) {
       this.name = initName; //constructor内ではプロパティ名の前にthis.をつける
       this.age = initAge;
+      //thisの中身は呼び出し方によって変わる
+      //constructor内のthisは呼び出しもとで生成されたインスタンスに置き換わる
     }
 
-    incrementAge() {
-      this.age += 1;
-    }
-    
+    // メソッド＝class内の関数
     // class内で作成するメソッドの第一引数にはthisの型を定義できる
     // thisの型をclass名にすることによって、間違った呼び出しを防止できる
+    incrementAge(this: Person) {
+      this.age += 1;
+    }
     greeting(this: Person) {
       console.log(`Hello! my name is ${this.name}.I am ${this.age} years old.`);
     }
+    //thisの中身は呼び出し方によって変わる
+    //メソッドの場合は呼び出し元の.メソッドの左側がthisに置き換わる
+    //インスタンス.メソッドでしかこのメソッドは呼べない仕組み（カプセル化）
   }
-  //new クラス名(constructor引数)でオブジェクト生成
+  //インスタンス＝classをもとに生成されるオブジェクト
+  //new クラス名(constructor引数)でインスタンス生成
   //それを定数quillに代入
   const quill = new Person('Quill', 38);
-
+  // インスタンスを用いてメソッドにアクセス
   quill.incrementAge();
-
-  //thisの中身は呼び出し方によって変わる
-  //constructor内のインスタンスがthisに置き換わる
-  //メソッドの場合は.の左側がメソッド内のthisに置き換わる
-  //つまりインスタンス.中身のメソッドでしかこのメソッドは呼べない
   quill.greeting();
 
-
+  // constructorの初期化処理を省略する方法
+  class Person2 {
+    // 以下のように引数に装飾子 プロパティ名:型注釈　とすることによって
+    // フィールド内と、constructor内を省略できる
+    constructor(public name: string, private age: number) {}
+    incrementAge(this: Person2) {
+      this.age += 1;
+    }
+    greeting(this: Person2) {
+      console.log(`Hello! my name is ${this.name}.I am ${this.age} years old.`);
+    }
+  }
+  const Jhon = new Person2('Jhon', 25);
+  quill.incrementAge();
+  quill.greeting();
 };
