3ef9e9c1c19fe1cb021cad31a929ccc22aea1365
hijiri <crew.sento0864@gmail.com>
2025-02-03 22:29:24 +0900

    TS学習６日目

--- learning-project/src/section/interface.ts
+++ learning-project/src/section/interface.ts
@@ -1,11 +1,25 @@
 export const interFace = () => {
   // interfaceはオブジェクトにのみ使える型を集約する機能
   // typeエイリアスのオブジェクトだけver.
-  interface Human {
+  interface Nameable {
     name: string;
+  }
+
+  // interfaceはextendsを使用でき、継承ができる
+  // 継承元はtypeエイリアスでもOK
+  // ただしプロパティ被りは継承元の型を満たしていないとエラー
+  interface Human extends Nameable {
     age: number;
     greeting(message: string): void; //メソッド型注釈の省略記法
   }
+
+  // typeエイリアスで継承したいときは&で指定する
+  // プロパティ被りは違う型を指定したらnever型となる
+  type Human2 = {
+    age: number;
+    greeting(message: string): void;
+  } & Nameable;
+
   const human: Human = {
     name: 'Quill',
     age: 38,
@@ -15,11 +29,12 @@ export const interFace = () => {
       );
     },
   };
+
   human.greeting('hello');
-  
+
   // implements interface型でinterfaceで
   // このクラスで生成するインスタンスのオブジェクトは
-  // 指定したinterfaceの型を少なくとも満たさないといけない
+  // 指定したinterfaceの型を少なくとも満たせば良い　※型の追加が可能
   class Developer implements Human {
     constructor(public name: string, public age: number) {}
     greeting(message: string) {
