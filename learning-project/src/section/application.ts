8401a585d24c1f32a30b69d3770f284ec5b8aeb4
SENTO <crew.sento0864@gmail.com>
2025-02-04 13:00:06 +0900

    Update application.ts

--- learning-project/src/section/application.ts
+++ learning-project/src/section/application.ts
@@ -1,4 +1,21 @@
 export const application=()=>{
+    type Engineer={
+        name:string;
+        role:string;
+    }
+
+    type Blogger={
+        name:string;
+        follower:number;
+    }
+
+    type EngineerBlogger=Engineer & Blogger;
+
+    const quill:EngineerBlogger=Engineer={
+        name:'Quill',
+        role:'front-end',
+        follower:1000,
+    }
 
     
 }
